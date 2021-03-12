3b9630fba93951f8813426d1cb4fcf3907cb8101
Sean Brown <seanbrown82@hotmail.com>
2021-03-12 01:10:46 +0000

    testing the api works

---
+++ main.js
@@ -0,0 +1,1 @@
+!async function(){const e=await fetch("http://api.openweathermap.org/data/2.5/weather?q=Manchester&appid=15ee7b1b8b63ce1967cb70787d8de6e7&units=metric",{mode:"cors"});console.log(e);const o=await e.json();console.log(o)}();
